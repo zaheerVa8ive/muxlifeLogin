7cb5a4ff94ae038509ec89a982158fb5fe8a4f95
zaheer008 <mahmedzaheer93@gmail.com>
2019-11-02 02:00:36 +0500

    redirect uri uncomment

--- src/app/views/sessions/signin/signin.component.ts
+++ src/app/views/sessions/signin/signin.component.ts
@@ -4,7 +4,7 @@ import { MatProgressBar, MatButton } from "@angular/material";
 import { MatSnackBar } from '@angular/material';
 import { Validators, FormGroup, FormControl } from "@angular/forms";
 import { SessionService } from "../session.service";
-import { Router , ActivatedRoute} from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-signin",
@@ -37,25 +37,25 @@ export class SigninComponent implements OnInit {
       password: new FormControl("", Validators.required),
       rememberMe: new FormControl(false)
     });
-    console.log("this.router",this.route);
-    console.log("this.router.url",this.route.url);
+    console.log("this.router", this.route);
+    console.log("this.router.url", this.route.url);
     this.route.url.subscribe(url => {
-      console.log("40 , url",url);
+      console.log("40 , url", url);
     });
-  //    this.route.params.subscribe(params => {
-  //     // this.id = +params['id']; // (+) converts string 'id' to a number
-  //     console.log("params",params);
-  //     // In a real app: dispatch action to load the details here.
-  //  });
+    //    this.route.params.subscribe(params => {
+    //     // this.id = +params['id']; // (+) converts string 'id' to a number
+    //     console.log("params",params);
+    //     // In a real app: dispatch action to load the details here.
+    //  });
     this.route
-    .params
-    .subscribe(params => {
-      console.log("params",params);
-      this.client_id = params['client_id'];
-      this.redirect_uri = params['redirect_uri'];
-      this.state = params['state'];
-      this.response_type = params['response_type'];
-    });
+      .params
+      .subscribe(params => {
+        console.log("params", params);
+        this.client_id = params['client_id'];
+        this.redirect_uri = params['redirect_uri'];
+        this.state = params['state'];
+        this.response_type = params['response_type'];
+      });
   }
 
   signin() {
@@ -68,21 +68,21 @@ export class SigninComponent implements OnInit {
       data => {
         if (data) {
           // localStorage.setItem("user", JSON.stringify(data));
-          console.log("data",data);
+          console.log("data", data);
           self._snackBar.openFromComponent(NotificationComponent, {
             data: 'You Are Succesfully Login !',
             duration: 2000
           });
-          console.log("this.client_id",this.client_id);
+          console.log("this.client_id", this.client_id);
           // window.location.href = `https://oauth-redirect.googleusercontent.com/r/lucky-calculator-c272b?code=${data.sessionToken}&state=${data.state}`;
           // window.location.href = `https://oauth-redirect.googleusercontent.com/r/lucky-calculator-c272b#access_token=${data.idToken}&token_type=bearer&state=${data.state}`;
           // var redirectURL = `https://oauth-redirect.googleusercontent.com/r/lucky-calculator-c272b?code=${data.sessionToken}&state=${self.state}`;
           var redirectURL2 = `https://oauth-redirect.googleusercontent.com/r/lucky-calculator-c272b#access_token=${data.sessionToken}&token_type=bearer&state=${self.state}`;
           // var redirectURL3 = `https://oauth-redirect.googleusercontent.com/r/lucky-calculator-c272b#access_token=${data.sessionToken}&token_type=bearer&state=${data.state}`;
           // console.log("redirectURL",redirectURL);
-          console.log("redirectURL2",redirectURL2);
+          console.log("redirectURL2", redirectURL2);
           // console.log("redirectURL2",redirectURL2);
-          // window.location.href = redirectURL2;
+          window.location.href = redirectURL2;
           // window.location.href = `https://oauth-redirect.googleusercontent.com/r/lucky-calculator-c272b?code=${data.idToken}&state=${data.state}`;
           // return;
         }
